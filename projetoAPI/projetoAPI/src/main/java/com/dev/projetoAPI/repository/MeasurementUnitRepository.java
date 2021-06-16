7ec4e946716d512a250ac7102d3187e403083818
Julyane Rocha <83929617+JulyNRocha@users.noreply.github.com>
2021-06-16 17:11:40 -0300

    Delete projetoAPI directory

--- projetoAPI/projetoAPI/src/main/java/com/dev/projetoAPI/repository/MeasurementUnitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dev.projetoAPI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dev.projetoAPI.model.MeasurementUnit;
-
-public interface MeasurementUnitRepository extends JpaRepository<MeasurementUnit, Integer> {
-
-}
